Fix errors in image resizing, loss calculation

nst/data.py
@@ -45,8 +45,10 @@ def load_image(image_fpath, image_size=None):
     image = tf.image.decode_image(image, channels=3, expand_animations=False)
     image = tf.image.convert_image_dtype(image, tf.float32)
     if image_size is not None:
+        image_shape = tf.shape(image)
+        height_and_width = tf.minimum(image_shape[0], image_shape[1])
+        image = tf.image.resize_with_crop_or_pad(image, height_and_width, height_and_width)
         image = tf.image.resize(image, image_size, preserve_aspect_ratio=True)
-        image = tf.image.resize_with_crop_or_pad(image, image_size[0], image_size[1])
     return image
 
 

nst/train.py
@@ -3,15 +3,13 @@ from tqdm import tqdm
 
 from nst.data import load_image, load_coco_2014_train
 from nst.models import StyleTransferNet, VGG16
-from nst.utils import normalize_image_batch, gram_matrix
 
 
 def train_model(args):
     vgg = VGG16()
     style_transfer_net = StyleTransferNet()
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
-    content_mse = tf.keras.losses.MeanSquaredError()
-    style_mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.NONE)
+    mse = tf.keras.losses.MeanSquaredError()
 
     checkpoint = tf.train.Checkpoint(style_transfer_net=style_transfer_net)
     checkpoint_manager = tf.train.CheckpointManager(checkpoint, args.ckpt_dir, max_to_keep=1)
@@ -20,9 +18,9 @@ def train_model(args):
     style_image = load_image(args.style_image_path, args.style_image_size)
     style_image = tf.expand_dims(style_image, axis=0)
     style_image = tf.repeat(style_image, repeats=args.batch_size, axis=0)
-    style_image = normalize_image_batch(style_image)
     style_features = vgg(style_image)
-    style_features_gram_matrix = {k: gram_matrix(v) for k, v in style_features.items()}
+    style_features_gram_matrix = {k: tf.reshape(gram_matrix(v), [args.batch_size, -1])
+                                  for k, v in style_features.items()}
 
     train_dataset = load_coco_2014_train(args.coco_img_dir, args.content_image_size, args.batch_size)
     n_batches = tf.data.experimental.cardinality(train_dataset).numpy()
@@ -30,8 +28,8 @@ def train_model(args):
     for epoch_id in range(args.epochs):
         with tqdm(desc=f'Training Epoch {epoch_id + 1}', unit='batch', total=n_batches) as pbar:
             for batch_id, X_batch in enumerate(train_dataset):
-                content_loss, style_loss, total_loss = train_step(vgg, style_transfer_net, optimizer, content_mse,
-                                                                  style_mse, X_batch, style_features_gram_matrix,
+                content_loss, style_loss, total_loss = train_step(vgg, style_transfer_net, optimizer, mse,
+                                                                  X_batch, style_features_gram_matrix,
                                                                   args.content_weight, args.style_weight)
                 if batch_id % args.ckpt_interval == 0 or batch_id == n_batches - 1:
                     checkpoint_manager.save()
@@ -44,26 +42,39 @@ def train_model(args):
 
 
 @tf.function(experimental_relax_shapes=True)
-def train_step(vgg, style_transfer_net, optimizer, content_mse, style_mse, X_batch, style_features_gram_matrix,
+def train_step(vgg, style_transfer_net, optimizer, mse, X_batch, style_features_gram_matrix,
                content_weight, style_weight):
     actual_batch_size = X_batch.shape[0]
     with tf.GradientTape() as tape:
         Y_batch = style_transfer_net(X_batch)
-        X_batch = normalize_image_batch(X_batch)
-        Y_batch = normalize_image_batch(Y_batch)
         features_X = vgg(X_batch)
         features_Y = vgg(Y_batch)
-        features_Y_gram_matrix = {k1: gram_matrix(v1) for k1, v1 in features_Y.items()}
+        features_Y_gram_matrix = {k: tf.reshape(gram_matrix(v), [actual_batch_size, -1])
+                                  for k, v in features_Y.items()}
 
-        content_loss = content_weight * content_mse(features_Y['relu2_2'], features_X['relu2_2'])
+        content_loss = content_weight * mse(
+            tf.reshape(features_Y['relu2_2'], [actual_batch_size, -1]),
+            tf.reshape(features_X['relu2_2'], [actual_batch_size, -1])
+        )
         style_loss = 0.0
-        for k1 in ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3']:
-            style_loss += tf.reduce_mean(style_mse(
-                features_Y_gram_matrix[k1], style_features_gram_matrix[k1][:actual_batch_size, :, :],
-            ))
+        for k in ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3']:
+            style_loss += mse(
+                features_Y_gram_matrix[k],
+                style_features_gram_matrix[k][:actual_batch_size, :],
+            )
         style_loss *= style_weight
         total_loss = content_loss + style_loss
 
         gradients = tape.gradient(total_loss, style_transfer_net.trainable_variables)
         optimizer.apply_gradients(zip(gradients, style_transfer_net.trainable_variables))
     return content_loss, style_loss, total_loss
+
+
+def gram_matrix(y):
+    batch_size, height, width, channels = y.shape
+    y = tf.transpose(y, [0, 3, 1, 2])
+    features = tf.reshape(y, (batch_size, channels, height * width))
+    features_t = tf.transpose(features, [0, 2, 1])
+    gram_mat = tf.matmul(features, features_t)
+    gram_mat /= channels * height * width
+    return gram_mat