Fix bugs in image loading, refactor loss calculation

nst/data.py
@@ -43,13 +43,12 @@ def load_coco_2014_train(images_dir, image_size, batch_size):
 def load_image(image_fpath, image_size=None):
     image = tf.io.read_file(image_fpath)
     image = tf.image.decode_image(image, channels=3, expand_animations=False)
-    image = tf.image.convert_image_dtype(image, tf.float32)
     if image_size is not None:
         image_shape = tf.shape(image)
         height_and_width = tf.minimum(image_shape[0], image_shape[1])
         image = tf.image.resize_with_crop_or_pad(image, height_and_width, height_and_width)
         image = tf.image.resize(image, image_size, preserve_aspect_ratio=True)
-    return image
+    return tf.math.round(image)
 
 
 def save_image(image, output_fpath):

nst/models.py
@@ -126,5 +126,5 @@ class ResidualConvBlock(tf.keras.layers.Layer):
 
 
 def create_instance_norm_layer():
-    return tfa.layers.InstanceNormalization(axis=-1, center=True, scale=True, beta_initializer='random_uniform',
-                                            gamma_initializer='random_uniform')
+    return tfa.layers.InstanceNormalization(axis=-1, center=True, scale=True, beta_initializer='zeros',
+                                            gamma_initializer='ones')

nst/train.py
@@ -19,8 +19,7 @@ def train_model(args):
     style_image = tf.expand_dims(style_image, axis=0)
     style_image = tf.repeat(style_image, repeats=args.batch_size, axis=0)
     style_features = vgg(style_image)
-    style_features_gram_matrix = {k: tf.reshape(gram_matrix(v), [args.batch_size, -1])
-                                  for k, v in style_features.items()}
+    style_features_gram_matrix = {k: gram_matrix(v) for k, v in style_features.items()}
 
     train_dataset = load_coco_2014_train(args.coco_img_dir, args.content_image_size, args.batch_size)
     n_batches = tf.data.experimental.cardinality(train_dataset).numpy()
@@ -49,18 +48,13 @@ def train_step(vgg, style_transfer_net, optimizer, mse, X_batch, style_features_
         Y_batch = style_transfer_net(X_batch)
         features_X = vgg(X_batch)
         features_Y = vgg(Y_batch)
-        features_Y_gram_matrix = {k: tf.reshape(gram_matrix(v), [actual_batch_size, -1])
-                                  for k, v in features_Y.items()}
 
-        content_loss = content_weight * mse(
-            tf.reshape(features_Y['relu2_2'], [actual_batch_size, -1]),
-            tf.reshape(features_X['relu2_2'], [actual_batch_size, -1])
-        )
+        content_loss = content_weight * mse(features_Y['relu2_2'], features_X['relu2_2'])
         style_loss = 0.0
         for k in ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3']:
             style_loss += mse(
-                features_Y_gram_matrix[k],
-                style_features_gram_matrix[k][:actual_batch_size, :],
+                gram_matrix(features_Y[k]),
+                style_features_gram_matrix[k][:actual_batch_size, :, :],
             )
         style_loss *= style_weight
         total_loss = content_loss + style_loss